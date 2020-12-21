90f32d57f558cecf77f6e982f3d41c2b73ca8e2f
Steveplays28 <62797992+Steveplays28@users.noreply.github.com>
2020-12-21 17:09:57 +0100

    Initial commit

---
+++ Assets/Scripts/ServerHandle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ServerHandle
+{
+    public static void WelcomeReceived(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ToString();
+
+        Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+
+        if (_fromClient != _clientIdCheck)
+        {
+            Debug.Log($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+        }
+
+        Server.clients[_fromClient].SendIntoGame(_username);
+    }
+
+    public static void UDPTestReceived(int _fromClient, Packet _packet)
+    {
+        string _msg = _packet.ReadString();
+
+        Debug.Log($"Received packet via UDP. Cotains message: {_msg}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ParkourFPS_Server
+ 
